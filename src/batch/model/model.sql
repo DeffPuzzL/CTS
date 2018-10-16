0a9e074ede8678b3361a676d1d1114c32e5fa226
deffpuzzl <deffpuzzl@qq.com>
2018-10-16 14:08:47 +0800

    version

--- src/batch/model/model.sql
+++
@@ -1,6 +0,0 @@
-alter table tbl_r_lmer_con add match_flag varchar2(1) default '0' not null;
-comment on column tbl_r_lmer_con.match_flag is '模糊匹配开关';
-alter table tbl_r_cha_con add match_flag varchar2(1) default '0' not null;
-comment on column tbl_r_cha_con.match_flag is '模糊匹配开关';
-alter table tbl_r_cmer_con add match_flag varchar2(1) default '0' not null;
-comment on column tbl_r_cmer_con.match_flag is '模糊匹配开关';
